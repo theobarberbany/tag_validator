f38d488b438faaf4f7d2957f6e165eee62883cf4
[Theo Barber-Bany] <theobarberbany@gmail.com>
2017-08-30 14:14:31 +0100

    Refactoring, adding argparse

--- validator.py
+++ validator.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python
 import sys
 import numpy as np
-#open file
-with open(sys.argv[1], 'r') as f:
-    read_data = f.read()
-print("Data passed : \n")
-print(read_data)
+import argparse
 
-#split input file into individual tags
-split = read_data.split()
+parser = argparse.ArgumentParser(description="Checks if  the supplied tags differ enough.")
+parser.add_argument("-f", "--file", dest='inputfile', ngargs=1,
+        help="pass a file containing tags to be checked")
+parser.add_argument("-d", "--database", type=str, nargs=2,
+        metavar=("USER","PASSWORD"),
+        help="toggle database mode")
 
-#check each tag is a valid combination of ATCG
+args = parser.parse_args()
+
+
+#function to check each tag is a valid combination of ATCG
 def check_bases(a_list):
     for tag in a_list:
         for base in tag:
@@ -20,9 +23,8 @@ def check_bases(a_list):
                 print("Tag {} contains invalid bases : {}".format(tag, base))
                 sys.exit()
 
-check_bases(split)
+#function to check for duplicate tags
 
-#check for duplicate tags
 def get_dups(a_list):
     duplicates = []
     for i in range(len(a_list)):
@@ -32,8 +34,46 @@ def get_dups(a_list):
     if len(duplicates) > 0:
         sys.exit("Duplicate Tag Found : {}   Quitting.".format(duplicates[0]))
 
-get_dups(split)
+#Function to compare two tags, returns the degree to which they differ.
+def difference(tag1, tag2):
+    if len(tag1) != len(tag2):
+        sys.exit('Tag length mismatch')
+    counter = 0
+    for char in range(len(tag1)):
+        if tag1[char] != tag2[char]:
+                counter +=1
+    return(counter)
 
+#Function to check if a list of tags differ by at least 3.
+def check_tags(tag_list):
+    for i in range(len(tag_list)):
+        for j in range(i+1, len(tag_list)):
+            dif = difference(tag_list[i],tag_list[j])
+            if dif < 3:
+               return("Comparing {} to {} : Insufficient difference of {} \n".format(tag_list[i],tag_list[j],dif))
+
+#File Mode
+
+if args.inputfile is not None:
+    #do some stuff when an inputfile is passed
+    with open(args.inputfile,'r') as f:
+        read_data = f.read()
+    print("Data passed : \n")
+    print(read_data)
+    
+    #split input file into individual tags
+    split = read_data.split()
+    #check the passed tags are valid combinations of ATCG
+    check_bases(split)
+    #check tags for duplicates
+    get_dups(split)
+    #check the entire list of tags
+    checked_tags = check_tags(split)
+    if checked_tags is not None:
+        print("Error in provided tags")
+        print(checked_tags)
+    else:
+        print("Tags ok")
 #arraytemp = np.array(split, dtype=bytes)
 #array = arraytemp.view('S1').reshape((arraytemp.size, -1))
 #print(repr(array))
@@ -46,27 +86,3 @@ get_dups(split)
 #counters = {}
 #for tagnum in range(len(split)):
 #    counters["tag{0}".format(tagnum)]=0
-
-#Function to compare two tags, returns the degree to which they differ.
-def difference(tag1, tag2):
-    if len(tag1) != len(tag2):
-        sys.exit('Tag length mismatch')
-    counter = 0
-    for char in range(len(tag1)):
-        if tag1[char] != tag2[char]:
-                counter +=1
-    return(counter)
-#Function to check if a list of tags differ by at least 3.
-def check_tags(tag_list):
-    for i in range(len(tag_list)):
-        for j in range(i+1, len(tag_list)):
-            dif = difference(tag_list[i],tag_list[j])
-            if dif < 3:
-               return("Comparing {} to {} : Insufficient difference of {} \n".format(tag_list[i],tag_list[j],dif))
-
-checked_tags = check_tags(split)
-if checked_tags is not None:
-    print("Error in provided tags")
-    print(checked_tags)
-else:
-    print("Tags ok")
