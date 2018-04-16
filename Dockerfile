d1ec6ca8f6572188c3415865168f65cafbaf1b92
[Theo Barber-Bany] <theobarberbany@gmail.com>
2018-04-16 09:35:48 +0100

    Add entrypoint

--- Dockerfile
+++ Dockerfile
@@ -6,3 +6,5 @@ COPY requirements.txt ./
 RUN pip install -r requirements.txt
 
 COPY . .
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# ------------------------------------------------------------
+# Environment Variables
+# ------------------------------------------------------------
+
+[[ -z ${TOKEN} ]] && echo "A valid TOKEN for accessing Vault was not specified in the environment." && exit 1
+[[ -z ${URL} ]] && echo "A valid URL for accessing Vault was not specified in the environment." && exit 1
+[[ -z ${INDEX} ]] && echo "A valid INDEX name for reading Vault secrets." && exit 1
+
+# -----------------------------------------------------------
+# Fetch Configuration From Vault
+# -----------------------------------------------------------
+
+override() {
+
+    # $1 the remote key to fetch from vault
+    # $2 the identifier in the file to replace with the value fetched from vault
+
+    echo "Vault Lookup: ${URL}/v1/secret/${INDEX}/$1 : $2"
+    json=$(curl -sH "X-Vault-Token:$TOKEN" -XGET ${URL}/v1/secret/${INDEX}/$1)
+    if [[ $? -eq 0 ]]; then
+        value=$(echo $json | jq -r .data.value)
+        [[ "${value}" != "null" ]] && echo "Vault Lookup: Override found for $2" && echo "${value}" && \
+        sed -i "s/$2/${value}/g" validator.py 
+    fi
+
+}
+
+override db_port DBPORT
+override db_name DBNAME
+override db_host DBHOST
+
+tail -f /dev/null
